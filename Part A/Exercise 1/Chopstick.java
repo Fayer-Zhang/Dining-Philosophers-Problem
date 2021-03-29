84ca5bb712a90020387e6d3016c2112957bd901a
Feier Zhang <fayerzhang0301@yahoo.com>
2021-03-29 11:31:34 -0400

    Finish PartA-Ex1

--- Part A/Exercise 1/Chopstick.java
+++ Part A/Exercise 1/Chopstick.java
@@ -1,19 +1,33 @@
 public class Chopstick {
-	private int ID;
-// hint: use a local variable to indicate whether the chopstick is free 
-//                        (lying on the table), e.g.  private boolean free;
+	private int 	ID;
+	private boolean free;
+	// hint: use a local variable to indicate whether the chopstick is free 
+	//                        (lying on the table), e.g.  private boolean free;
 
 	Chopstick(int ID) {
-		  this.ID = ID;
-	
+		this.ID = ID;
+		free = true;	
 	}
 	
 	synchronized void take() {
-	
+		while(!free)
+		{
+			try
+			{
+				wait();
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println(e);
+			}
+		}
+
+		free = false;
 	}
 	
 	synchronized void release() {
-	
+		free =  true;
+		notify();
 	}
 	    
 	public int getID() {
